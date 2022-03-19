08e99bfcf9e1000f87898a66dc771bc8a225a13e
manuel-janeta <96266517+manuel-janeta@users.noreply.github.com>
2022-03-19 11:28:50 -0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+archivo_excel = pd.read_excel("Data.xls")
+
+st.title("Aplicación movil")
+st.sidebar.title("Parámetros")
+
+seleccionador = st.sidebar.selectbox(
+    "Seleccione un módulo", ["Módulo 1", "Módulo 2", "Módulo 3", "Módulo 4"])
+if seleccionador == "Módulo 1":
+    st.write("Usted está en el Módulo 1")
+    st.write(archivo_excel)
+    figura = px.line_3d(archivo_excel,x="Eje X",y="Eje Y",z="Eje Z")
+    st.write(figura)
+elif seleccionador == "Módulo 2":
+    st.write("Usted está en el Módulo 2")
+elif seleccionador == "Módulo 3":
+    st.write("Usted está en el Módulo 3")
+else:
+    st.write("Usted está en el Módulo 4")
+
